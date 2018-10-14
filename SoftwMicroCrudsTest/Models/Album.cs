4589d9f171c34fe76174d90814533aa6b61862f8
manoelcontatosi@gmail.com <manoelcontatosi@gmail.com>
2018-10-14 11:58:02 -0300

    AdminSS SoftwMicro - Módulo Albuns - Desenvolvimento MVC e Front end.

---
+++ SoftwMicroCrudsTest/Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace SoftwMicroCrudsTest.Models
+{
+    public class Album
+    {
+        private int Codigo { get; set; }
+
+        private string Nome { get; set; }
+
+        private List<Foto> Fotolist { get; set; }
+
+        private Image Imagem { get; set; }
+
+        private string Link { get; set; }
+    }
+}
---
+++ SoftwMicroCrudsTest/Models/Foto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoftwMicroCrudsTest.Models
+{
+    public class Foto
+    {
+        private int Codigo { get; set; }
+
+        private string Tipo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private string Tamanho { get; set; }
+
+        private string Local { get; set; }
+
+    }
+}
